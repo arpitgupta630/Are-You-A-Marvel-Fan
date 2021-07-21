38d666a81a48c1b3bdc722b153f0694697cd6992
Arpit Gupta <arpitguptamitrc@gmail.com>
2021-07-21 23:09:12 +0530

    feat: function added

--- marvelQuizGame.js
+++ marvelQuizGame.js
@@ -10,3 +10,15 @@ console.log('Initial Score:', score);
 
 console.log('\n:RULES:\n1. Right Answer will increase 2 Marks and Worng Answer will deducte 1 Mark.\n2. Case Sensitive Input.');
 
+function quiz(question, answer) {
+    console.log('\nJarvis:', question);
+    let userAnswer = readlineSync.question(userName + ': ');
+    if (userAnswer === answer){
+        score = score + 2;
+        console.log('Bravo!!!', userName,'\b, Right Answer \nNew Score:', score);
+    } else{
+        score--;
+        console.log('Oops!!! Worng Answer\nNew Score:', score);
+    }
+}
+
